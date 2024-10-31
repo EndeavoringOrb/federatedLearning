054893aac4c2108d4b2cf22b8165f7ea3d954e4e
Aaron B <aaronbelikoff@gmail.com>
2024-10-31 13:07:07 -0400

    add testing for normally-trained model

--- trainNormal.py
+++ trainNormal.py
@@ -1,7 +1,8 @@
 import torch
-from tqdm import tqdm
+import numpy as np
 import torch.nn as nn
 import torch.optim as optim
+from time import perf_counter
 
 from shakespeareData.shakespeareData import tokenLoader
 
@@ -26,8 +27,8 @@ class SimpleRNN(nn.Module):
         data = torch.randn((hiddenSize, vocabSize)) * 0.02
         self.ho = nn.Parameter(data)
 
-    def getInitState(self):
-        return self.initState
+    def getInitState(self, batchSize):
+        return self.initState.unsqueeze(0).expand((batchSize, -1))
 
     def forward(self, state, token):
         for i in range(self.nLayers):
@@ -38,24 +39,50 @@ class SimpleRNN(nn.Module):
     def out(self, state):
         return state @ self.ho
 
+    @torch.no_grad
+    def preprocess(self, tokens):
+        state = self.getInitState(1)
+        for token in tokens:
+            state = self.forward(state, token)
+        return state
+
+    @torch.no_grad
+    def generate(self, state, stopToken):
+        newTokens = []
+        while True:
+            out = self.out(state)
+            probs = torch.softmax(out, dim=-1)
+            newToken = torch.multinomial(probs, 1)[0].item()
+            if newToken == stopToken:
+                break
+            newTokens.append(newToken)
+            state = self.forward(state, newToken)
+        return newTokens
+
 
 if __name__ == "__main__":
     # SETTINGS
     ##########
-    hiddenSize = 16
+    hiddenSize = 32
     vocabSize = 76
     nLayers = 4
 
-    batchSize = 2
+    batchSize = 16
     learningRate = 1e-4
+
+    saveInterval = 10
     ##########
 
     print(f"Loading data")
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     tokenGenerator = tokenLoader(vocabSize, batchSize)
 
     print(f"Initializing model")
-    model = SimpleRNN(vocabSize, hiddenSize, nLayers).to(device)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    device = torch.device("cpu")
+    print(f"Using {device}")
+    # model: SimpleRNN = SimpleRNN(vocabSize, hiddenSize, nLayers).to(device)
+    model: SimpleRNN = torch.load("model.pt", weights_only=False).to(device)
+    print(f"Model has {sum(p.numel() for p in model.parameters()):,} parameters")
 
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=learningRate)
@@ -63,32 +90,82 @@ if __name__ == "__main__":
     stepNum = 0
 
     print(f"Training")
-    tokens, batchInfo = next(tokenGenerator)
-    batchTokens = []
-    for length in batchInfo:
-        batchTokens.append(tokens[:length])
-        tokens = tokens[length:]
     while True:
+        # Get tokens
+        tokens, batchInfo = next(tokenGenerator)
+        batchTokens = []
+        for length in batchInfo:
+            batchTokens.append(tokens[:length])
+            tokens = tokens[length:]
         batchTokens = [
             torch.tensor(sequence, dtype=torch.int64, device=device)
             for sequence in batchTokens
         ]
 
+        # Batch forward pass
         loss = 0.0
         batchNumTokens = sum(batchInfo)
+        chunkLengths = torch.tensor(batchInfo, dtype=torch.int64)
+        sortedChunkLengths = sorted(list(set(batchInfo)))
+        indices = torch.arange(batchSize)
+        state = model.getInitState(batchSize).to(device)
 
-        for sequence in batchTokens:
+        start = perf_counter()  # Start timer
+
+        previousLength = 0
+        for length in sortedChunkLengths:
+            # Remove sequences that are finished
+            # TODO: optimize this by doing a nested for loop with the chunkLengths
+            removeIndices = torch.where(chunkLengths == previousLength)[0]
+            notRemoveIndices = torch.where(chunkLengths != previousLength)[0]
+            if len(removeIndices) > 0:
+                removeIndices, _ = torch.sort(removeIndices, descending=True)
+                for idx in removeIndices:
+                    batchTokens.pop(idx)
+                chunkLengths = chunkLengths[notRemoveIndices]
+                state = state[notRemoveIndices]
+                indices = np.arange(len(chunkLengths))
+
+            for i in range(previousLength, length):
+                # Get tokens for current step
+                currentTokens = torch.tensor(
+                    [chunk[i] for chunk in batchTokens],
+                    dtype=torch.int64,
+                    device=device,
+                )
+
+                # Get preds
+                out = model.out(state)
+
+                # Get loss
+                loss += criterion(out, currentTokens)
+
+                # Get next state
+                state = model(state, currentTokens)
+
+            previousLength = length
+
+        """for sequence in batchTokens:
             state = model.getInitState().to(device)
 
-            for token in tqdm(sequence):
+            for token in sequence:
                 out = model.out(state)
                 loss += criterion(out, token)
-                state = model(state, token)
+                state = model(state, token)"""
 
+        # Backward pass
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
+        elapsed = perf_counter() - start
+
+        # Tracking
         stepNum += 1
 
-        print(f"Step: {stepNum:,}, Loss: {loss / batchNumTokens}")
+        # Logging
+        if stepNum % saveInterval == 0:
+            torch.save(model, "model.pt")
+        print(
+            f"Step: {stepNum:,}, Loss: {loss / max(batchInfo):.4e}, {int(batchNumTokens / elapsed):,} tok/sec"
+        )
